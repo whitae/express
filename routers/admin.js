53e5b630bf7bbdd0eb5e01b3984643bf387daaff
whitae <35728340+whitae@users.noreply.github.com>
2021-03-15 17:17:10 +0800

    123

--- routers/admin.js
+++ routers/admin.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+// 引入模块
+const users = require('./admin/users');
+
 let router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('后台管理');
 });
+router.use('/users', users);
 module.exports = router;
---
+++ routers/admin/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+let router = express.Router();
+router.get('/', (req, res) => {
+    res.send('用户管理首页');
+});
+router.get('/add', (req, res) => {
+    res.send('添加用户');
+});
+router.get('/delete', (req, res) => {
+    res.send('删除用户');
+});
+module.exports = router;
